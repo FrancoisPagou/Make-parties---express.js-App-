5ff094c282f490c1c3f6c369229ddbba9c28a893
FrancoisPagou <larrylalong21@gmail.com>
2022-06-30 22:36:45 +0100

    Users can see all mock events

--- app.js
+++ app.js
@@ -13,10 +13,23 @@ app.engine('handlebars', engine({ defaultLayout: 'main', handlebars: allowInsecu
 app.set('view engine', 'handlebars');
 
 
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+];
+
 // render home page
 app.get('/', (req, res) => {
-    res.render('home', {msg: 'Handlebars'});
+    res.render('event-index', {events});
 });
+
+app.get('/events', (req, res) => {
+    res.render('event-index', {events});
+});
+
+
 
 //Choose your port 
 const port = process.env.PORT || 3000;
