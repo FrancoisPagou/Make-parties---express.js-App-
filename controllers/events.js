6091f569125dda90c90e514e4744e3e4c36725d9
FrancoisPagou <pagoufrancois@gmail.com>
2022-07-21 20:58:49 +0200

    added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = function(app, models) {
     // render home page
     app.get('/', (req, res) => {
@@ -33,13 +35,15 @@ module.exports = function(app, models) {
 
     // SHOW
    app.get('/events/:id', (req, res) => {
-        models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
-            console.log("RSVPS ", event.Rsvp)
-            res.render('events-show', { event: event });
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    });
+    models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+        let createdAt = event.createdAt;
+        createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        event.createdAtFormatted = createdAt;
+        res.render('events-show', { event: event });
+    }).catch((err) => {
+        console.log(err.message);
+    })
+});
 
     // EDIT
     app.get('/events/:id/edit', (req, res) => {
